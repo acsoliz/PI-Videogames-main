ac0091970399f8d43cfa6148a0d2490b3a991f73
acsoliz <acsoliz@outlook.com>
2022-03-30 01:40:31 +0200

    nose que probar
    detalles ??

--- api/src/db.js
+++ api/src/db.js
@@ -5,35 +5,33 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        poot: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: http//github.com/brianc/node-postgres/inssues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
 
-
- :new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-	logging : false, // set to console.log to see the raw SQL queries
-	native  : false // lets Sequelize know we can use pg-native for ~30% more speed
-});
+		process.env.NODE_ENV === 'production' ? new Sequelize({
+			database       : DB_NAME,
+			dialect        : 'postgres',
+			host           : DB_HOST,
+			port           : 5432,
+			username       : DB_USER,
+			password       : DB_PASSWORD,
+			pool           : {
+				max  : 3,
+				min  : 1,
+				idle : 10000
+			},
+			dialectOptions : {
+				ssl       : {
+					require            : true,
+					// Ref.: http//github.com/brianc/node-postgres/inssues/2009
+					rejectUnauthorized : false
+				},
+				keepAlive : true
+			},
+			ssl            : true
+		}) :
+		new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+			logging : false,
+			native  : false
+		});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
