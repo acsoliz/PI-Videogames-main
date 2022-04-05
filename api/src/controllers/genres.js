b4d2dac44b21c69ce4b9846c28b55cb2c926145c
acsoliz <acsoliz@outlook.com>
2022-04-05 12:59:03 +0200

    get Genres
    se modifico la ruta /genres, agregamos que guarde los generos del end point

--- api/src/controllers/genres.js
+++ api/src/controllers/genres.js
@@ -4,10 +4,31 @@ const { Op } = require('sequelize');
 const router = express();
 const { Videogame, Genre } = require('../db');
 const { API_KEY } = process.env;
+const API_GENRES = 'https://api.rawg.io/api/genres?key=';
 
 //______________________getAllGenres_ ______________________________________
 
 async function getAllGenres(req, res, next) {
+	const AllGenres = await axios(`${API_GENRES}${API_KEY}`);
+
+	const genres =
+		AllGenres &&
+		AllGenres.data.results.map((e) => {
+			return {
+				name : e.name
+			};
+		});
+	for (let genre of genres) {
+		Genre.create({
+			name : genre.name
+		});
+	}
+
+	try {
+	} catch (error) {
+		console.log('ERROR AL CREAR DB GENRES', error);
+	}
+
 	const { genre } = req.query;
 	try {
 		let allgenres = await Genre.findAll({ attributes: [ 'name' ] });
@@ -19,5 +40,28 @@ async function getAllGenres(req, res, next) {
 		console.log(error);
 	}
 }
+async function getApiGenres() {}
+// const maxId = await Videogame.max('id');
+// // console.log(maxId, "soy el id");
+// if (!maxId) {
+// 	const AllGenres = await axios(`${API_GENRES}${API_KEY}`);
 
-module.exports = { getAllGenres };
+// 	const genres =
+// 		AllGenres &&
+// 		AllGenres.data.results.map((e) => {
+// 			return {
+// 				name : e.name
+// 			};
+// 		});
+// 	for (let genre of genres) {
+// 		Genre.create({
+// 			name : genre.name
+// 		});
+// 	}
+
+// 	try {
+// 	} catch (error) {
+// 		console.log('ERROR AL CREAR DB GENRES', error);
+// 	}
+
+module.exports = { getAllGenres, getApiGenres };
